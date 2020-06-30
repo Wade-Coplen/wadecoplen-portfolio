af4d2ea5c179a91a33ac457072f045a4ae2b5a0e
Wade-Coplen <wadecoplen@gmail.com>
2020-06-29 19:35:29 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -5,20 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    {/* all routes here */}
+     {/* //project 1 route
+     //project 2 route
+     //project 3 route */}
     </div>
   );
 }
